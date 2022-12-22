1783fb7a395d9b97d99e048209c1f15371c5f0a6
ffjjgih <lamcaobui26@gmail.com>
2022-12-23 01:03:18 +0700

    dev system transaction bill

---
+++ src/main/java/com/vibee/model/response/DownloadFilePdfResponse.java
@@ -0,0 +1,13 @@
+package com.vibee.model.response;
+
+public class DownloadFilePdfResponse extends BaseResponse {
+    private String resource;
+
+    public String getResource() {
+        return resource;
+    }
+
+    public void setResource(String resource) {
+        this.resource = resource;
+    }
+}
